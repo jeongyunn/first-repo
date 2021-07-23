fc12e5d25bc4ad9912473d4ceff35a8536555a28
yunn <yunn@localhost>
2021-07-23 21:19:37 +0900

    최초 커밋.

---
+++ project1/src/com/kh/exam1/HelloJava.java
@@ -0,0 +1,15 @@
+package com.kh.exam1;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		System.out.print("Hello Java Programming");
+		//한 줄에 출력
+		
+		System.out.println("Hello Java Programming"); //ln = line의 약자
+		//한 줄에 출력후 개행(엔터) :출력 후 엔터 
+		
+		
+	}
+
+}
